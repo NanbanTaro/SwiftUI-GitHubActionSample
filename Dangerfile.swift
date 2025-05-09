092acb978f7aab7c481020f5185ea6dcd494d574
NanbanTaro <chicken.de.nanban@gmail.com>
2025-05-09 17:09:19 +0900

    静的解析を追加

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,8 +1,13 @@
 import Danger
 
 let danger = Danger()
-let modified = danger.git.modifiedFiles + danger.git.createdFiles
+// let modified = danger.git.modifiedFiles + danger.git.createdFiles
 
-if !modified.contains("CHANGELOG.md") {
-    warn("CHANGELOG.md が更新されていません。")
-}
+// if !modified.contains("CHANGELOG.md") {
+//     warn("CHANGELOG.md が更新されていません。")
+// }
+
+SwiftLint.lint(
+    inline: true,
+    configFile: ".swiftlint.yml"
+)
