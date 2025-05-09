3619de395d1caed4dd9240ea7370c327367be5b4
NanbanTaro <chicken.de.nanban@gmail.com>
2025-05-09 11:57:25 +0900

    SwiftLint導入

---
+++ GitHubActionSampleApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GitHubActionSampleApp
+//
+//  Created by NanbanTaro on 2025/05/09.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubActionSampleApp/GitHubActionSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  GitHubActionSampleApp.swift
+//  GitHubActionSampleApp
+//
+//  Created by NanbanTaro on 2025/05/09.
+//  
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
