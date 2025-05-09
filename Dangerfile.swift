3e852ec5dc8d3700ac77b7ed0a4aac80feb75442
NanbanTaro <chicken.de.nanban@gmail.com>
2025-05-09 16:28:40 +0900

    テスト

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,7 +1,7 @@
 import Danger
 
 let danger = Danger()
-let modified = danger.git.modifiedFiles + dander.git.createdFiles
+let modified = danger.git.modifiedFiles + danger.git.createdFiles
 
 if !modified.contains("CHANGELOG.md") {
     warn("CHANGELOG.md が更新されていません。")
